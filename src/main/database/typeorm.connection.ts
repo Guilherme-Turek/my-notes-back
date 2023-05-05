d5187c44daa42e94224fd4de031a72175a07db55
Guilherme Turek <guilherme.lsturek@gmail.com>
2023-05-05 14:17:21 -0300

    conexão db

--- src/main/database/typeorm.connection.ts
+++ src/main/database/typeorm.connection.ts
@@ -4,12 +4,15 @@ import config from "../config/typeorm.config";
 export class TypeormConnection {
   private static _connection: DataSource;
 
-  public static async init() {
+  public static async connect() {
     this._connection = await config.initialize();
     console.log("Typeorm is ready.");
   }
 
   public static get connection() {
+    if (!this._connection) {
+      throw new Error("db not connected");
+    }
     return this._connection;
   }
 }
--- src/main/server/express.server.ts
+++ src/main/server/express.server.ts
@@ -1,9 +1,18 @@
 import { serverEnv } from "../../app/envs/server.envs";
 import { createApp } from "../config/express.config";
+import { TypeormConnection } from "../database/typeorm.connection";
+import { Express } from "express";
 
 export class Server {
+  private static app: Express;
+
   public static run() {
-    const app = createApp();
-    app.listen(serverEnv.port, () => console.log("Server is running."));
+    Server.app = createApp();
+
+    Promise.all([TypeormConnection.connect()]).then(this.listen);
+  }
+
+  private static listen() {
+    Server.app.listen(serverEnv.port, () => console.log("Server is running."));
   }
 }
